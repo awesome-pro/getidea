9491a6a50e945bbd97427fb7c380513ed31db62b
abhinandan-verma <abhinandanverma551@gmail.com>
2024-08-24 01:05:47 +0530

    login look

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,8 +2,10 @@ import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
   return (
-    <div className='w-full min-h-screen flex items-center justify-center bg-[url("/image.png")] bg-cover bg-center'>
-      <SignIn />
+    <div className='w-full min-h-screen flex items-center justify-around bg-black bg-[url("/image.png")] bg-cover bg-center gap-0'>
+        <SignIn />
+        <div className='lg:w-[600px] w-0'>
+        </div>
     </div>
   )
 }
